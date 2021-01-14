353a06386aa83dd0d4ff6535601173411c03a132
maximilienpy <max314116@gmail.com>
2021-01-14 06:42:36 +0100

    adding layout Axentix , and Role users added

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -16,9 +16,7 @@ class UsersController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('users/index.html.twig', [
-            'controller_name' => 'UsersController',
-        ]);
+        return $this->render('users/index.html.twig');
     }
 
     /**
