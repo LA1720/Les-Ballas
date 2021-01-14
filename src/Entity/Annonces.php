d7acb12f31e206571762940323569aae7f3eae0e
ALI AFZAL <layaafzal74@gmail.com>
2021-01-14 04:30:39 +0100

    slug,datetime,ajout

--- src/Entity/Annonces.php
+++ src/Entity/Annonces.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use App\Repository\AnnoncesRepository;
 use Doctrine\ORM\Mapping as ORM;
 
+
+
 /**
  * @ORM\Entity(repositoryClass=AnnoncesRepository::class)
  */
@@ -23,6 +25,7 @@ class Annonces
     private $title;
 
     /**
+     * @Gedmo\Mapping\Annotation\Slug(fields={"title"})
      * @ORM\Column(type="string", length=255)
      */
     private $slug;
@@ -33,6 +36,7 @@ class Annonces
     private $content;
 
     /**
+     * @Gedmo\Mapping\Annotation\Timestampable(on="create")
      * @ORM\Column(type="datetime")
      */
     private $created_at;
@@ -86,12 +90,7 @@ class Annonces
         return $this->slug;
     }
 
-    public function setSlug(string $slug): self
-    {
-        $this->slug = $slug;
-
-        return $this;
-    }
+   
 
     public function getContent(): ?string
     {
@@ -110,12 +109,7 @@ class Annonces
         return $this->created_at;
     }
 
-    public function setCreatedAt(\DateTimeInterface $created_at): self
-    {
-        $this->created_at = $created_at;
-
-        return $this;
-    }
+    
 
     public function getActive(): ?bool
     {
---
+++ src/Form/AnnoncesType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Annonces;
+use App\Entity\Categories;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class AnnoncesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('neuf', CheckboxType::class, [
+                'label' => 'Neuf',
+                'attr' => array('checked'),
+            ])
+            ->add('prix', IntegerType::class)
+            ->add('content', TextareaType::class)
+            ->add('categories', EntityType::class, [
+                'class' => Categories::class 
+            ])
+            ->add('Valider', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Annonces::class,
+        ]);
+    }
+}
